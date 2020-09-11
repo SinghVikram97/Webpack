80d8e7273f0546e3d17e0f3c77226794823aa194
Vikram <vikram.bedi@paytm.com>
2020-09-11 13:05:42 +0530

    Added button styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
 const button = makeButton("Yay! A Button!");
+button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
 
 console.log(

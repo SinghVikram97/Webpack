bff4ea4001199694fcc6e1167ea0f9bce0c8d7a6
Vikram <vikram.bedi@paytm.com>
2020-09-16 12:04:57 +0530

    Added dynamic import for footer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import nav from "./nav";
-import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
 import imageUrl from "./webpack-logo.jpg";
 import css from "./footer.css";
 import buttonStyles from "./button.css";
+// import  {footer}  from "./footer";
+const loadFooter=()=> import("./footer.js")
 
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 
+button.addEventListener("click",e=>{
+  loadFooter().then((m)=>{
+    document.body.appendChild(m.footer);
+  })
+})
+
 document.body.appendChild(button);
 document.body.appendChild(image);
-document.body.appendChild(footer);
+// document.body.appendChild(footer);
